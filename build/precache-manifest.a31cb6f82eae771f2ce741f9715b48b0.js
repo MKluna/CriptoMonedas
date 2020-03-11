d9c10a614d634bc47ad0b566ebc34b79a8ea29f6
unknown <mkluna@institutosanmartin.edu.ar>
2020-03-11 02:35:02 -0300

    Primer commit

---
+++ build/precache-manifest.a31cb6f82eae771f2ce741f9715b48b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51fbd13cb76d582aaf14efe38caf2056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f5e7d1af8537e9ad610",
+    "url": "/static/css/main.cd2a2a20.chunk.css"
+  },
+  {
+    "revision": "18577844c8c726b98e10",
+    "url": "/static/js/2.575fc223.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.575fc223.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f5e7d1af8537e9ad610",
+    "url": "/static/js/main.556c9344.chunk.js"
+  },
+  {
+    "revision": "3fc4155feb15f57bbb2c",
+    "url": "/static/js/runtime-main.ea334e17.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
